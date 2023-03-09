e32ef69587be96690cfe8d571590a999a0e2042b
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-03-08 17:55:26 -0800

    Added components

--- sobaditsgood/src/app/app.module.ts
+++ sobaditsgood/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { AddReviewComponent } from './add-review/add-review.component';
+import { DisplayReviewComponent } from './display-review/display-review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    MovieInfoComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    AddReviewComponent,
+    DisplayReviewComponent
   ],
   imports: [
     BrowserModule,
