9a3a2c6eda276481127c4720b19f280fdd46e6e7
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-03-09 22:44:21 -0800

    Create account page finished

--- sobaditsgood/src/app/app-routing.module.ts
+++ sobaditsgood/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 const routes: Routes = [
   {path:'userprofile', component:UserProfileComponent},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'createaccount', component:CreateAccountComponent}
 ];
 
 @NgModule({
