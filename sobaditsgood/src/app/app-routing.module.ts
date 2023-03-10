0ec1c8db9d6d50fd63033d87462d9ce2b63b1f68
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-03-09 22:21:11 -0800

    login template finished

--- sobaditsgood/src/app/app-routing.module.ts
+++ sobaditsgood/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'userprofile', component:UserProfileComponent}
+  {path:'userprofile', component:UserProfileComponent},
+  {path:'login', component:LoginComponent}
 ];
 
 @NgModule({
