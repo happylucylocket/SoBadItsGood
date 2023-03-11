8ab4372a7facb13c3ca8929a7277aa78bba58dab
Sarah <happylucylocket123@hotmail.com>
2023-03-10 19:32:35 -0800

    Fix the fix of merge conflict

--- sobaditsgood/src/app/apiservice.service.ts
+++ sobaditsgood/src/app/apiservice.service.ts
@@ -5,23 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class APIServiceService {
+  baseUrl:string = "http://localhost:3000"
 
-<<<<<<< HEAD
   constructor(private http: HttpClient) { }
+
+  // for testing purposes
+  getData(){
+    return this.http.get(this.baseUrl+'/api', {responseType: 'text'});
+  }
 
   // Format movie name using '+' for spaces (ie: 'Mission+Impossible')
   searchMovie(movieName: string) {
     return this.http.get<Object>('https://api.themoviedb.org/3/search/movie?api_key=ecd28fb4488e17f072d95ad0278f2545&query='+ movieName).subscribe((data:any)=>{
       console.log(data);
     });
-=======
-  baseUrl:string = "http://localhost:3000"
-
-  constructor(private http:HttpClient){}
-
-  // for testing purposes
-  getData(){
-    return this.http.get(this.baseUrl+'/api', {responseType: 'text'});
->>>>>>> a01c650173a87ac1bce5908cde8fa0d02ea38a80
   }
 }
