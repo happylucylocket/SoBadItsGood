041934af77e3c709e51a90444de3375536933ef6
Sarah <happylucylocket123@hotmail.com>
2023-03-10 19:41:34 -0800

    Pls be the end of this merging hell

--- sobaditsgood/src/app/app.component.ts
+++ sobaditsgood/src/app/app.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-=======
 import { Component, OnInit } from '@angular/core';
->>>>>>> a01c650173a87ac1bce5908cde8fa0d02ea38a80
 import { APIServiceService } from './apiservice.service';
 
 @Component({
@@ -10,16 +6,7 @@ import { APIServiceService } from './apiservice.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
-export class AppComponent {
-  tempMovie: any;
-  data: any;
-  constructor(private apiService: APIServiceService) {
-    this.data = this.apiService.searchMovie('The+Room');
-    this.tempMovie = this.data;
-  }
 
-=======
 export class AppComponent implements OnInit {
   title = 'So Bad Its Good';
 
@@ -30,5 +17,4 @@ export class AppComponent implements OnInit {
       console.log(data);
     });
   }
->>>>>>> a01c650173a87ac1bce5908cde8fa0d02ea38a80
 }
